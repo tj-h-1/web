5dfa40b2212dcdfa792ce85391feb20ef9d08ff4
tj-h-1 <hibbinstj@gmail.com>
2026-04-25 22:20:42 +0100

    code cleanup: removed duplicate code in calculator.js for buttons 1-8 and unused classes in calculator.html

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -21,149 +21,129 @@ const clearBtn = document.getElementById("clearBtn");
 let calcString = String(document.getElementById("expression"));
 
 oneBtn.addEventListener("click", function() {
-    calcString += "1"
-    document.getElementById("expression").innerHTML += "1"
-    console.log(calcString)
+    calcString += "1";
+    document.getElementById("expression").innerHTML += "1";
 
 });
 
 twoBtn.addEventListener("click", function() {
-    calcString += "2"
-    document.getElementById("expression").innerHTML += "2"
-    console.log(calcString)
+    calcString += "2";
+    document.getElementById("expression").innerHTML += "2";
 
 });
 
 threeBtn.addEventListener("click", function() {
-    calcString += "3"
-    document.getElementById("expression").innerHTML += "3"
-    console.log(calcString)
+    calcString += "3";
+    document.getElementById("expression").innerHTML += "3";
 
 });
 
 fourBtn.addEventListener("click", function() {
-    calcString += "4"
-    document.getElementById("expression").innerHTML += "4"
-    console.log(calcString)
+    calcString += "4";
+    document.getElementById("expression").innerHTML += "4";
 
 });
 
 fiveBtn.addEventListener("click", function() {
-    calcString += "5"
-    document.getElementById("expression").innerHTML += "5"
-    console.log(calcString)
+    calcString += "5";
+    document.getElementById("expression").innerHTML += "5";
 
 });
 
 sixBtn.addEventListener("click", function() {
-    calcString += "6"
-    document.getElementById("expression").innerHTML += "6"
-    console.log(calcString)
+    calcString += "6";
+    document.getElementById("expression").innerHTML += "6";
 
 });
 
 sevenBtn.addEventListener("click", function() {
-    calcString += "7"
-    document.getElementById("expression").innerHTML += "7"
-    console.log(calcString)
+    calcString += "7";
+    document.getElementById("expression").innerHTML += "7";
 
 });
 
 eightBtn.addEventListener("click", function() {
-    calcString += "8"
-    document.getElementById("expression").innerHTML += "8"
-    console.log(calcString)
+    calcString += "8";
+    document.getElementById("expression").innerHTML += "8";
 
 });
 
 nineBtn.addEventListener("click", function() {
-    calcString += "9"
-    document.getElementById("expression").innerHTML += "9"
-    console.log(calcString)
+    calcString += "9";
+    document.getElementById("expression").innerHTML += "9";
 
 });
 
 zeroBtn.addEventListener("click", function() {
-    calcString += "0"
-    document.getElementById("expression").innerHTML += "0"
-    console.log(calcString)
+    calcString += "0";
+    document.getElementById("expression").innerHTML += "0";
 
 });
 
 addBtn.addEventListener("click", function() {
     calcString += "+";
-    document.getElementById("expression").innerHTML += "+"
-    console.log(calcString);
+    document.getElementById("expression").innerHTML += "+";
 
 });
 
 subBtn.addEventListener("click", function() {
-    calcString += "-"
-    document.getElementById("expression").innerHTML += "-"
-    console.log(calcString)
+    calcString += "-";
+    document.getElementById("expression").innerHTML += "-";
 
 });
 
 multBtn.addEventListener("click", function() {
-    calcString += "*"
-    document.getElementById("expression").innerHTML += "*"
-    console.log(calcString)
+    calcString += "*";
+    document.getElementById("expression").innerHTML += "*";
 
 });
 
 divBtn.addEventListener("click", function() {
-    calcString += "/"
-    document.getElementById("expression").innerHTML += "/"
-    console.log(calcString)
+    calcString += "/";
+    document.getElementById("expression").innerHTML += "/";
 
 });
 
 decimalBtn.addEventListener("click", function() {
-    calcString += "."
-    document.getElementById("expression").innerHTML += "."
-    console.log(calcString)
+    calcString += ".";
+    document.getElementById("expression").innerHTML += ".";
 
 });
 
 clearBtn.addEventListener("click", function() {
     document.getElementById("expression").innerHTML = ""
-    calcString = ""
-
-    console.log(document.getElementById("expression").innerHTML)
-    console.log(calcString)
+    calcString = "";
+    
 
 });
 
 
 
 calcBtn.addEventListener("click", function() {
-    document.getElementById("result").innerHTML = ""
+    document.getElementById("result").innerHTML = "";
 
     let [num1, operator, num2] = calcString.match(/-?\d+\.?\d*|[+\-*/]/g);
-    console.log(num1)
-    console.log(operator)
-    console.log(num2)
-    const finalNum1 = Number(num1)
-    const finalNum2 = Number(num2)
+    const finalNum1 = Number(num1);
+    const finalNum2 = Number(num2);
 
     var result; 
 
     if (operator == "+") {
-        result = finalNum1 + finalNum2
+        result = finalNum1 + finalNum2;
 
     } else if (operator == "*") {
-        result = finalNum1 * finalNum2
+        result = finalNum1 * finalNum2;
 
     } else if (operator == "-") {
-        result = finalNum1 - finalNum2
+        result = finalNum1 - finalNum2;
 
     }else if (operator == "/") {
-        result = finalNum1 / finalNum2
+        result = finalNum1 / finalNum2;
     }
     
     
     document.getElementById("result").innerHTML += `=${result}`;
-    console.log(result)
+
 });
 
 
